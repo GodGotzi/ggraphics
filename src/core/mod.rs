d7028cf51b740917214844df57dfcaf8e0fbff66
GodGotzi <eli.gottsbacher@gmail.com>
2023-03-24 14:46:18 +0100

    as

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,8 +1,4 @@
 pub mod component;
 pub mod container;
 pub mod screen;
-
-pub struct Div {
-    parent: Option<&Component>,
-    childs: Vec<Component>,
-}
+pub mod window;
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,59 @@
-mod style;
 
-use pronto_graphics::Window;
-use pronto_graphics::Color;
+mod style;
+mod core;
+
+use crate::core::window::{ State };
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("test - window")
+        .build(&event_loop).unwrap();
+
+    let mut state = State::new(window).await;
+
+    event_loop.run(move |event, _, control_flow| 
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == window.id() => if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        // new_inner_size is &&mut so we have to dereference it twice
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    );
+}
 
 fn main() {
-    let mut pg = Window::new(800, 600, "Circles!");
+    run();
 
-    loop {
-        pg.circle((100., 100.), 15.);
-        pg.fill_color(Color::BLUE);
-        pg.circle((300., 450.), 24.);
-        
-        pg.update();
-    }
+    pollster::block_on(run());
 }
